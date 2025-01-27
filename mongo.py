70617d4954362a55d698c28e67e5c8182494dca8
Mahipalsingh6695 <mahipalsinghchauhan1995@gmail.com>
2025-01-27 17:23:06 +0530

    we

--- mongo.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://mahipalsinghchauhan1995:Mahi%406695@tipsg.8e47a.mongodb.net/?retryWrites=true&w=majority&appName=TipsG")
-db = client["tipsG"]
-results_collection = db["results"]
-registration_collection = db["registrations"]
